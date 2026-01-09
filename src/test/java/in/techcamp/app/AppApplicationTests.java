3c607f2dce6a077dfefbc14bc3f20f462263145e
m-inazawa <m.inazawa@infomart.co.jp>
2026-01-09 10:54:04 +0900

    first commit

---
+++ src/test/java/in/techcamp/app/AppApplicationTests.java
@@ -0,0 +1,13 @@
+package in.techcamp.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
