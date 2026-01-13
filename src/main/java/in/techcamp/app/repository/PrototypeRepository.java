74e3fed2397702d5afe39ca3d7f50c4618a9ab03
morichi8 <y.mori@infomart.co.jp>
2026-01-13 15:58:57 +0900

    リポジトリのSELECT文に、ユーザーID・ユーザー名も含めてプロトタイプの投稿を取得するための記述を追加

--- src/main/java/in/techcamp/app/repository/PrototypeRepository.java
+++ src/main/java/in/techcamp/app/repository/PrototypeRepository.java
@@ -3,6 +3,8 @@ package in.techcamp.app.repository;
 import java.util.List;
 
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Result;
+import org.apache.ibatis.annotations.Results;
 import org.apache.ibatis.annotations.Select;
 
 import in.techcamp.app.entity.PrototypeEntity;
@@ -10,6 +12,10 @@ import in.techcamp.app.entity.PrototypeEntity;
 @Mapper
 public interface PrototypeRepository {
 
-  @Select("SELECT * FROM prototypes")
+  @Select("SELECT p.*, u.id AS user_id, u.name AS user_name FROM prototypes p JOIN users u ON p.user_id = u.id ORDER BY p.created_at DESC")
+  @Results(value = {
+    @Result(property = "user.id", column = "user_id"),
+    @Result(property = "user.name", column = "user_name")
+  })
   List<PrototypeEntity> findAll();
 }
