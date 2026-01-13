8e4f6cd6e1b36ba80e63f831d6f318ab5689fd6a
morichi8 <y.mori@infomart.co.jp>
2026-01-13 15:29:15 +0900

    リポジトリのSELECT文追記

--- src/main/java/in/techcamp/app/repository/PrototypeRepository.java
+++ src/main/java/in/techcamp/app/repository/PrototypeRepository.java
@@ -1,8 +1,15 @@
 package in.techcamp.app.repository;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import in.techcamp.app.entity.PrototypeEntity;
 
 @Mapper
 public interface PrototypeRepository {
-  
+
+  @Select("SELECT * FROM prototypes")
+  List<PrototypeEntity> findAll();
 }
