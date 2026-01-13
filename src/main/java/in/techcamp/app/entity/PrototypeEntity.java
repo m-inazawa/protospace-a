36805c8e5abbc0b5b953b8264ec304187fd66866
morichi8 <y.mori@infomart.co.jp>
2026-01-13 14:15:30 +0900

    エンティティのフィールド作成

--- src/main/java/in/techcamp/app/entity/PrototypeEntity.java
+++ src/main/java/in/techcamp/app/entity/PrototypeEntity.java
@@ -1,8 +1,17 @@
 package in.techcamp.app.entity;
 
+import java.sql.Timestamp;
+
 import lombok.Data;
 
 @Data
 public class PrototypeEntity {
-  
+  private Integer id;
+  private String prototypeName;
+  private String concept;
+  private String catchCopy;
+  private String image;
+  private Timestamp createdAt;
+  private Timestamp updatedAt;
+  private UserEntity user;
 }
