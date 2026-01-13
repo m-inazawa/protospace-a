75259c8e4cba870afa4f879c3a96bf07925a8900
morichi8 <y.mori@infomart.co.jp>
2026-01-13 15:57:05 +0900

    コントローラーのフィールド作成

--- src/main/java/in/techcamp/app/controller/PrototypeController.java
+++ src/main/java/in/techcamp/app/controller/PrototypeController.java
@@ -1,8 +1,24 @@
 package in.techcamp.app.controller;
 
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import in.techcamp.app.entity.PrototypeEntity;
+import in.techcamp.app.repository.PrototypeRepository;
+import lombok.AllArgsConstructor;
 
 @Controller
+@AllArgsConstructor
 public class PrototypeController {
-  
+  private final PrototypeRepository prototypeRepository;
+
+  @GetMapping("/")
+  public String showPrototypes(Model model) {
+    List<PrototypeEntity> prototypes = prototypeRepository.findAll();
+    model.addAttribute("prototypes", prototypes);
+    return "prototype/index";
+  }
 }
