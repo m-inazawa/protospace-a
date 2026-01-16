45029e39f8cabfc20c61ba2211decac7133e6cc4
m-inazawa <m.inazawa@infomart.co.jp>
2026-01-16 15:56:11 +0900

    ユーザー新規登録の単体テスト作成・タイポ修正

--- src/main/java/in/techcamp/app/service/UserService.java
+++ src/main/java/in/techcamp/app/service/UserService.java
@@ -1,14 +1,14 @@
 package in.techcamp.app.service;
 
-import org.springframework.stereotype.Service;
 import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.validation.BindingResult;
 
+import in.techcamp.app.entity.UserEntity;
+import in.techcamp.app.form.RegisterForm;
 import in.techcamp.app.repository.UserRepository;
 import lombok.AllArgsConstructor;
-import in.techcamp.app.form.RegisterForm;
-import in.techcamp.app.entity.UserEntity;
 
 @Transactional
 @Service
@@ -32,17 +32,14 @@ public class UserService {
 
     UserEntity userEntity = new UserEntity();
     userEntity.setEmail(registerForm.getEmail());
-    System.out.println("★この先のエラーはUserService以降の処理内容に関連したエラーです。");
 
     String encodedPassword = passwordEncoder.encode(registerForm.getPassword());
-    System.out.println("★encodedパスワード: " + encodedPassword);
     userEntity.setPassword(encodedPassword);
 
     userEntity.setUserName(registerForm.getUserName());
     userEntity.setProfile(registerForm.getProfile());
     userEntity.setAffiliation(registerForm.getAffiliation());
     userEntity.setPosition(registerForm.getPosition());
-    System.out.println("★Formから取得した内容をSet後のuserEntityの内容(この時点で、idとcreatedAtがnullであることは問題ないです。):" + userEntity);
     userRepository.insertUser(userEntity);
   }
 }
