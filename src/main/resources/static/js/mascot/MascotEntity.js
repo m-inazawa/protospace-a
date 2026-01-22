a508f353e817eda4764f134fb6647d34e1276092
m-inazawa <m.inazawa@infomart.co.jp>
2026-01-22 10:40:11 +0900

    ・猫の画像の透過・画像サイズ調整・画像転換速度調整

--- src/main/resources/static/js/mascot/MascotEntity.js
+++ src/main/resources/static/js/mascot/MascotEntity.js
@@ -1,9 +1,12 @@
 // キャラクターのスペック定義
+
+const DEFAULT_FRAME_COUNT = { n: 2, s: 2, e: 2, w: 2, ne: 2, nw: 2, se: 2, sw: 2 };
+
 export const CharacterConfigs = {
-    CAT: {
-        id: 'cat',
-        basePath: '/images/characters/cat/',
-        frameCount: 4, // 1方向につき4枚
-        interval: 150  // コマ送りの速度（ミリ秒）
-    }
+  CAT: {
+    id: 'cat',
+    basePath: '/images/characters/cat/',
+    frameCount: { ...DEFAULT_FRAME_COUNT },
+    interval: 150  // コマ送りの速度（ミリ秒）
+  }
 };
